0c23efbad3bc498e41f192d39a9f4159f76113d4
Yogender <yashmittal92002@gmail.com>
2024-01-08 12:22:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const inputbox=document.getElementById("input-box");
+const listitem=document.getElementById("list-item");
+
+function addTask()
+{
+    if(inputbox.value==='')
+    {
+        alert("You must write something");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputbox.value;
+        listitem.appendChild(li);
+        let span= document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value="";
+    saveData();
+}
+
+listitem.addEventListener("click",function(e){
+    if(e.target.tagName==="LI")
+    {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data",listitem.innerHTML);
+}
+function showTask(){
+    listitem.innerHTML=localStorage.getItem("data");
+}
+
+showTask();
